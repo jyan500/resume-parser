a89a09b571efffa3885c603e4eb0f99a99dd1f8b
jyan500 <jyan500@gmail.com>
2026-02-17 18:27:53 -0800

    added some more potential section headers

--- utils/constants.py
+++ utils/constants.py
@@ -2,5 +2,10 @@ SECTION_PATTERNS = {
     'summary': r'(?i)^(summary|profile|objective|about|professional summary).*$',
     'experience': r'(?i)^(experience|work history|employment|professional experience|work experience).*$',
     'education': r'(?i)^(education|academic background|academic).*$',
-    'skills': r'(?i)^(skills|technical skills|competencies|core competencies).*$'
+    'skills': r'(?i)^(skills|technical skills|competencies|core competencies).*$',
+    'projects': r'(?i)^(projects).*$',
+    'interests': r'(?i)^(interests|hobbies).*$',
+    'accomplishments': r'(?i)^(accomplishments|achievements).*$',
+    'certifications': r'(?i)^(certifications).*$',
+    'languages': r'(?i)^(languages|foreign languages).*$',
 }
