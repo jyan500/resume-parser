995db0e21b585e3c935efcdb8a58232a45581958
jyan500 <jyan500@gmail.com>
2026-03-13 21:45:09 -0700

    deleted test

--- server/test_models.py
+++
@@ -1,7 +0,0 @@
-from utils.ner_parser_v2 import NerResumeParserV2
-from utils.models_v2 import Models
-
-models = Models()
-parser = NerResumeParserV2(models.ner, models.ner_dates, models.zero_shot_classifier, models.tagger) 
-
-parser.parse_job_history(["Software Engineer", "Sep 2021 - Jul 2023", "Rhumbix", "Remote", "knex.js", "Collaborate with product and development teams to ship new features."])
